6ea00e20910441a69b855fedc2dd29ed83be3963
trevor ngugi <ngugitrevor@gmail.com>
2020-07-10 21:25:44 +0300

    add admin page

---
+++ src/main.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+using namespace std;
+
+void home();
+void adminLogin();
+
+int main(){
+
+    home();
+
+    return 0;
+}
+
+void home(){
+    int choice;
+
+    cout<<"**** WELCOME TO BBIT2020 E-CASH SERVICES ****\n";
+    cout<<" 1. normal user login \n";
+    cout<<" 2. administrator login \n \n";
+    cout<<" choose an option to proceed : "; 
+    cin >> choice;
+
+    if (choice==1){
+        // normal user login
+        cout<<"user login \n";
+    }
+    else if(choice==2){
+        adminLogin();
+    }
+    else{
+        cout<<" wrong option choose 1 or 2 \n";
+    }
+}
+
+void adminLogin(){
+    string username ,password;
+    cout<<" welcome to registration and deposit page \n";
+    cout<<" enter administrator  username and password \n";
+    cout<<" enter username : ";
+    cin>>username;
+    cout<<" enter password : ";
+    cin>>password;
+
+    if(username=="admin"&&password=="admin123"){
+        // admin page
+        cout<<"admin page";
+    }
+    else{
+        cout<<"wrong username or password \n \n";
+        home();
+    }
+}
+
